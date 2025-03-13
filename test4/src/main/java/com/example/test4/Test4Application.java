83d650a77a24acab5f23281e3ad3a7ad1dc5c8f3
jyotiranjanbarik2001 <158537486+jyotiranjanbarik2001@users.noreply.github.com>
2025-03-13 16:25:08 +0530

    Update Test4Application.java

--- test4/src/main/java/com/example/test4/Test4Application.java
+++ test4/src/main/java/com/example/test4/Test4Application.java
@@ -8,6 +8,11 @@ public class Test4Application {
 
 	public static void main(String[] args) {
 		int x=4000;
+		int y=82723;
+		float x=23.3;
+		double x=89.32;
+		long=283787238;
+		
 		SpringApplication.run(Test4Application.class, args);
 	}
 
